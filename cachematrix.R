9b2f1cd36c7a6859187aa9018be1dd0a2d0d7f07
ongbak77 <pierre@andurandcapital.com>
2017-07-01 23:28:15 +0100

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the functions makecacheMatrix and cacheSolve calculate the inverse of a 
+## matrix without calculating it more than once as the result is stored in
+## the cache. This exercise is to practice the scoping rules in R
+## the functions would be called as below: (with the results)
+## > aMatrix<-makecacheMatrix(matrix(c(2,0,0,2), nrow=2,ncol=2))
+## > cacheSolve(aMatrix)
+##       [,1] [,2]
+## [1,]  0.5  0.0
+## [2,]  0.0  0.5
+## > cacheSolve(aMatrix)
+## getting cached data
+##       [,1] [,2]
+## [1,]  0.5  0.0
+## [2,]  0.0  0.5
+##
 
-## Write a short comment describing this function
+## makecacheMatrix puts the input matrix x in the list format, 
+## but does not calculate the inverse 
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makecacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  ## x has to be in format makecacheMatrix
+  ## it will give the message "getting cache data" if it has already been calculated
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
